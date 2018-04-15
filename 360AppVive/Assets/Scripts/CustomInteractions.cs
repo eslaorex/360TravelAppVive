6b42b83c0ae4e2e8a9e5e43d84749f0145f42ce0
Eslao rex <eslao_rex@yahoo.com>
2018-04-15 23:14:55 +0200

    closeSunday

---
+++ 360AppVive/Assets/Scripts/CustomInteractions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CustomInteractions : VRTK_InteractableObject
+{
+    public GameObject canvaS;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log("esta usado");
+        canvaS.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
