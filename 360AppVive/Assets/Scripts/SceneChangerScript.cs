09f8bee20604d6ba3b93204b8fe4f16894f10473
Eslao rex <eslao_rex@yahoo.com>
2018-04-16 17:08:56 +0200

    home

---
+++ 360AppVive/Assets/Scripts/SceneChangerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.SceneManagement;
+
+public class SceneCaveScript : VRTK_InteractableObject
+{
+    public GameObject sceneCave01;
+    public VRTK_ControllerEvents vRTK_ControllerEvents;
+    private bool sceneCaveLaberintEnter;
+    public int levelToLoad2;
+
+    private void Start()
+    {
+       
+        vRTK_ControllerEvents.TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+    }
+
+    private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        if(sceneCaveLaberintEnter)
+        {
+            SceneManager.LoadScene(levelToLoad2);
+        }
+        
+    }
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+
+        base.StartUsing(usingObject);
+        {
+            if (!sceneCave01.activeInHierarchy)
+            {
+                sceneCave01.SetActive(true);
+                sceneCaveLaberintEnter = true;
+
+            }
+        }
+    }
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+
+        base.StopUsing(usingObject);
+        {
+            if (sceneCave01.activeInHierarchy)
+            {
+                sceneCave01.SetActive(false);
+                sceneCaveLaberintEnter = false;
+
+            }
+        }
+    }
+
+
+}
