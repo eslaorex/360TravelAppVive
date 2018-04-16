3ccf76a1e5c87ff3128d1f961af906ac0fe831e2
eslaorex <38095691+eslaorex@users.noreply.github.com>
2018-04-16 12:33:55 +0200

    Test04

---
+++ 360AppVive/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MainMenuScript : VRTK_InteractableObject
+{
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<VRTK_ControllerEvents>().GripPressed += new ControllerInteractionEventHandler(DoGripPressed);
+        GetComponent<VRTK_ControllerEvents>().ButtonTwoPressed += new ControllerInteractionEventHandler(DoButtonTwoPressed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void DoGripPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("grippressed");
+    }
+
+
+    private void DoButtonTwoPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("button2 pressed");
+    }
+
+
+}
--- 360AppVive/Assets/Scripts/changeScene.cs
+++ 360AppVive/Assets/Scripts/changeScene.cs
@@ -15,6 +15,7 @@ public class changeScene : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+
 	}
 
     public void SceneChanger()
